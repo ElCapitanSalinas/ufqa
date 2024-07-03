5d511ef40fc9d47ee365112f485a1e4628cc9677
ElCapitanSalinas <63880943+ElCapitanSalinas@users.noreply.github.com>
2024-07-02 22:45:38 -0500

    \

---
+++ ufqaweb/static/admin/tailwind.config 2.js
@@ -0,0 +1,24 @@
+tailwind = {}
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        rojo: '#FF004D',
+        rojodark: '#8C002A',
+        graynav: '#F4F4F6',
+      },
+      fontFamily: {
+        sans : ["Red Hat Display"]
+      },
+      lineHeight: {
+        '12': '6rem',
+      },
+      spacing: {
+        '10': '100px',
+      },
+    }
+  },
+}
+
+  
